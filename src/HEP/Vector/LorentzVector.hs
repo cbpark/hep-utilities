746255c7701b15d214a6ef10e9d9095767370431
cbpark <cbpark@gmail.com>
2014-09-13 02:33:14 +0200

    Add some documentation

--- src/HEP/Vector/LorentzVector.hs
+++ src/HEP/Vector/LorentzVector.hs
@@ -1,20 +1,38 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  HEP.Vector.LorentzVector
+-- Copyright   :  (c) 2014 Chan Beom Park
+-- License     :  BSD-style
+-- Maintainer  :  Chan Beom Park <cbpark@gmail.com>
+-- Stability   :  experimental
+-- Portability :  GHC
+--
+-- This module provides a simple Lorentz vector type and utility functions for
+-- four-momentum objects in high-energy processes.
+--
+--------------------------------------------------------------------------------
 module HEP.Vector.LorentzVector
-    (
-      LorentzVector (..)
+       ( -- * Type
+         LorentzVector (..)
 
-    , setXYZT
-    , setEtaPhiPtM
-    , vectorSum
-    , invariantMass
-    , transverseMass
-    , eta
-    , phi
-    , pT
-    , deltaPhi
-    , deltaR
-    , deltaTheta
-    , boostVector
-    ) where
+         -- * Function
+       , setXYZT
+       , setEtaPhiPtM
+
+       , vectorSum
+       , invariantMass
+       , transverseMass
+
+       , eta
+       , phi
+       , pT
+
+       , deltaPhi
+       , deltaR
+       , deltaTheta
+
+       , boostVector
+       ) where
 
 import           Control.Applicative       (Applicative (..))
 import           Data.Foldable             (Foldable (..))
@@ -31,18 +49,9 @@ import qualified HEP.Vector.ThreeVector    as V3
 import           HEP.Vector.TwoVector      (TwoVector)
 import qualified HEP.Vector.TwoVector      as V2
 
+-- | The Lorentz vector type.
 newtype LorentzVector a = LorentzVector { getVector :: V4 a }
                         deriving (Eq, Ord, Show)
-
-setXYZT :: a -> a -> a -> a -> LorentzVector a
-setXYZT px' py' pz' e' = LorentzVector (V4 e' px' py' pz')
-
-setEtaPhiPtM :: Floating a => a -> a -> a -> a -> LorentzVector a
-setEtaPhiPtM eta' phi' pt' m' = LorentzVector (V4 e px py pz)
-  where e = sqrt $ px * px + py * py + pz * pz + m' * m'
-        px = pt' * cos phi'
-        py = pt' * sin phi'
-        pz = pt' * sinh eta'
 
 instance Functor LorentzVector where
   fmap f (LorentzVector (V4 t x y z)) = LorentzVector (V4 (f t) (f x) (f y) (f z))
@@ -59,13 +68,29 @@ instance Metric LorentzVector where
   (LorentzVector (V4 t x y z)) `dot` (LorentzVector (V4 t' x' y' z')) =
     t * t' - x * x' - y * y' - z * z'
 
+-- | Makes 'LorentzVector' out of values based on x, y, z, t coordinates.
+setXYZT :: a -> a -> a -> a -> LorentzVector a
+setXYZT px' py' pz' e' = LorentzVector (V4 e' px' py' pz')
+
+-- | Makes 'LorentzVector' out of values based on pseudorapidity, azimuthal angle,
+-- transverse momentum, and mass coordinates.
+setEtaPhiPtM :: Floating a => a -> a -> a -> a -> LorentzVector a
+setEtaPhiPtM eta' phi' pt' m' = LorentzVector (V4 e px py pz)
+  where e = sqrt $ px * px + py * py + pz * pz + m' * m'
+        px = pt' * cos phi'
+        py = pt' * sin phi'
+        pz = pt' * sinh eta'
+
+-- | Vector sum of Lorentz vectors.
 vectorSum :: (Foldable f, Functor f, Num a)
              => f (LorentzVector a) -> LorentzVector a
 vectorSum = LorentzVector . sumV . fmap getVector
 
+-- | Invariant mass.
 invariantMass :: Floating a => LorentzVector a -> a
 invariantMass = norm
 
+-- | Trasverse mass.
 transverseMass :: Floating a => LorentzVector a -> LorentzVector a -> a
 transverseMass = TV.invariantMass `on` transverseV
     where transverseV (LorentzVector (V4 t x y z)) =
@@ -74,29 +99,36 @@ transverseMass = TV.invariantMass `on` transverseV
 transV :: LorentzVector a -> TwoVector a
 transV (LorentzVector (V4 _ x y _)) = V2.TwoVector (V2 x y)
 
+-- | Magnitude of transverse momentum.
 pT :: Floating a => LorentzVector a -> a
 pT = norm . V2.getVector . transV
 
 spatialV :: Num a => LorentzVector a -> ThreeVector a
 spatialV (LorentzVector (V4 _ x y z)) = V3.ThreeVector (V3 x y z)
 
+-- | Pseudorapidity.
 eta :: (Floating a, Ord a) => LorentzVector a -> a
 eta = V3.pseudoRapidity . spatialV
 
+-- | Azimuthal angle.
 phi :: RealFloat a => LorentzVector a -> a
 phi = V3.phi . spatialV
 
+-- | Azimuthal angle difference between two Lorentz vectors.
 deltaPhi :: RealFloat a => LorentzVector a -> LorentzVector a -> a
 deltaPhi v v' = V2.phi2MPiPi $ phi v - phi v'
 
+-- | Size of the cone spanned by two Lorentz vectors.
 deltaR :: RealFloat a => LorentzVector a -> LorentzVector a -> a
 deltaR v v' = sqrt $ deta * deta + dphi * dphi
     where deta = eta v - eta v'
           dphi = deltaPhi v v'
 
+-- | Separation angle between two Lorentz vectors.
 deltaTheta :: (Floating a, Ord a) => LorentzVector a -> LorentzVector a -> a
 deltaTheta = V3.angle `on` spatialV
 
+-- | Boost vector. It returns 'ThreeVector'.
 boostVector :: Fractional a => LorentzVector a -> ThreeVector a
 boostVector v@(LorentzVector (V4 t _ _ _)) =
   V3.ThreeVector ((V3.getVector . spatialV) v ^/ t)
