33a0b626541ad444fc048775ee8bf9544708e4e7
cbpark <cbpark@gmail.com>
2017-12-13 20:33:46 +0900

    Update nix and stack files

--- shell.nix
+++ shell.nix
@@ -1,33 +1,16 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
 
 let
 
   inherit (nixpkgs) pkgs;
 
-  f = { mkDerivation, attoparsec, base, bytestring, lens, linear
-      , mwc-random, pipes, pipes-attoparsec, pipes-bytestring, stdenv
-      , transformers, vector
-      }:
-      mkDerivation {
-        pname = "hep-utilities";
-        version = "0.1.0.0";
-        src = ./.;
-        isLibrary = true;
-        isExecutable = false;
-        libraryHaskellDepends = [
-          attoparsec base bytestring lens linear mwc-random pipes
-          pipes-attoparsec pipes-bytestring transformers vector
-        ];
-        homepage = "https://github.com/cbpark/hep-utilities";
-        description = "Utilities for analyzing high energy physics data";
-        license = stdenv.lib.licenses.bsd3;
-      };
-
   haskellPackages = if compiler == "default"
                        then pkgs.haskellPackages
                        else pkgs.haskell.packages.${compiler};
 
-  drv = haskellPackages.callPackage f {};
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage ./default.nix {});
 
 in
 
