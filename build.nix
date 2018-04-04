34fad5a9c309e158aa1482eb90f8a5603dc91d65
cbpark <cbpark@gmail.com>
2018-04-04 19:30:21 +0900

    Remove stack.yaml, default.nix and shell.nix. Use build.nix

---
+++ build.nix
@@ -0,0 +1,12 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+  inherit (nixpkgs) pkgs;
+
+  haskellPackages = if compiler == "default"
+                      then pkgs.haskellPackages
+                      else pkgs.haskell.packages.${compiler};
+
+in
+  { hep-utilities = haskellPackages.callCabal2nix "hep-utilities" ./. {};
+  }
